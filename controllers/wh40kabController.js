941b356d0b368eef8e733f881bd2f1702063f71a
Angelo Onofrio <enrimaggio@gmail.com>
2017-10-29 12:20:00 +0100

    battle roles

--- controllers/wh40kabController.js
+++ controllers/wh40kabController.js
@@ -1,6 +1,19 @@
 exports.getArmyIndex = function(req, res) {
-    var list = [{ id: 1, name: "Sammael", role: '1' }, { id: 2, name: "Ezekiel", role: '1' }];
-    res.json(list);
+    let sql = 'SELECT * FROM BattlefieldRoles';
+    var list = [];
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            list.push({ id: row.id, name: row.name });
+            console.log(row.name);
+        });
+        res.json(list);
+    });
+    //var list = [{ id: 1, name: "Sammael", role: '1' }, { id: 2, name: "Ezekiel", role: '1' }];
+
+
 
     // Task.findById(req.params.armyId, function(err, task) {
     //     if (err)
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const sqlite3 = require('sqlite3').verbose();
 bodyParser = require('body-parser');
 
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -15,7 +16,7 @@ app.use(bodyParser.json());
 var routes = require('./routes/wh40kabRoutes'); //importing route
 routes(app); //register the route
 
-let db = new sqlite3.Database('./db/ArmyList.sqlite', (err) => {
+db = new sqlite3.Database('./db/ArmyList.sqlite', (err) => {
     if (err) {
         console.error(err.message);
     }
