ae8ad6a159f0b68aa45cadb8ead72e3f92a0f637
Angelo Onofrio <enrimaggio@gmail.com>
2017-10-29 11:47:37 +0100

    add sqlite support

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
+
+const sqlite3 = require('sqlite3').verbose();
+
 bodyParser = require('body-parser');
 
 
@@ -12,8 +15,12 @@ app.use(bodyParser.json());
 var routes = require('./routes/wh40kabRoutes'); //importing route
 routes(app); //register the route
 
+let db = new sqlite3.Database('./db/ArmyList.sqlite', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    app.listen(port);
 
-
-app.listen(port);
-
-console.log('wh40karmy builder RESTful API server started on: ' + port);
+    console.log('wh40karmy builder RESTful API server started on: ' + port);
+    console.log('Connected to the armylist database.');
+});
